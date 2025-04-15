18c4381522376133623b59518cdc84fdd78e1c51
aradhana148 <nira.aradhana7@gmail.com>
2025-04-15 16:11:39 +0530

    converts uploaded to csv

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import os
+import subprocess
+
 
 app=Flask(__name__)
 
-@app.route('/')
+UPLOADFOLDER = "uploads"
+os.makedirs(UPLOADFOLDER,mode=0o777, exist_ok=True)
+@app.route('/',methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        file=request.files['logfile']
+        file.save(file.filename)
+        os.rename(file.filename, UPLOADFOLDER+'/'+file.filename)  
+        subprocess.run(['bash','awking.sh', UPLOADFOLDER+'/'+file.filename], check=True)   
     return render_template('log_upload.html')
 
 if __name__ == "__main__":
